2ae408eaf528dac056d8fcf099dd8c674e77abcd
GR4NDIVI4ST3R <ihsxaviorp@gmail.com>
2019-12-04 14:02:26 -0800

    Empty input update and btn disabled update

--- main.js
+++ main.js
@@ -12,64 +12,73 @@ function keyboard() {
     document.getElementById("keyboard").innerHTML = layout;
 }
 
-function isEmpty(str) {
-    return !str.trim().length;
-}
-
 function submit() {
     phrase = "";
     input = document.getElementById("input").value;
-    for (var i = 0; i < input.length; i+= 1) {
-        if (input.charAt(i).match(/^[a-zA-Z]+$/)) { /* The /^[]+$/ is Javascript Regex */
-            phrase += "___ ";
-        }
-        /* If [else if] isn't used, the last [else] will only apply to the [if] above it */
-        else if (input.charAt(i) == " ") {
-            phrase += "     ";
-        }
-        else if (input.charAt(i) == "'") {
-            phrase += "' ";
-        }
-        else if (input.charAt(i) == "-") {
-            phrase += "— ";
-        }
-        else if (input.charAt(i) == ".") {
-            phrase += ". ";
-        }
-        else if (input.charAt(i) == ",") {
-            phrase += ", ";
-        }
-        else if (input.charAt(i) == "?") {
-            phrase += "? ";
-        }
-        else if (input.charAt(i) == "!") {
-            phrase += "! ";
-        }
-        else if (input === "__invalid__") {
-            alert("Invalid Phrase; Please use the following: a-z, A-Z, \" ? \", \" ! \", \" \' \" or \" . \"");
-            phrase = "DON'T LET THE GUESSER SEE IT!";
-            input = "__invalid__";
-            break;
-        }
-        else {
-            alert("Invalid Phrase; Please use the following: a-z, A-Z, \" ? \", \" ! \", \" \' \" or \" . \"");
-            phrase = "DON'T LET THE GUESSER SEE IT!";
-            input = "__invalid__";
-            break;
+    if (input.trim().length === 0) {
+        alert("huh");
+        alert("Invalid Phrase; Please use the following: [ a-z ], [ A-Z ], [ – ], [ . ], [ \' ], [ ? ] or [ ! ]");
+        phrase = "DON'T LET THE GUESSER SEE IT!";
+        input = "__invalid__";
+    }
+    else {
+        for (var i = 0; i < input.length; i+= 1) {
+            if (input.charAt(i).match(/^[a-zA-Z]+$/)) { /* The /^[]+$/ is Javascript Regex */
+                phrase += "___ ";
+            }
+            /* If [else if] isn't used, the last [else] will only apply to the [if] above it */
+            else if (input.charAt(i) == " ") {
+                phrase += "     ";
+            }
+            else if (input.charAt(i) == "'") {
+                phrase += "' ";
+            }
+            else if (input.charAt(i) == "-") {
+                phrase += "— ";
+            }
+            else if (input.charAt(i) == ".") {
+                phrase += ". ";
+            }
+            else if (input.charAt(i) == ",") {
+                phrase += ", ";
+            }
+            else if (input.charAt(i) == "?") {
+                phrase += "? ";
+            }
+            else if (input.charAt(i) == "!") {
+                phrase += "! ";
+            }
+            else {
+                alert("Invalid Phrase; Please use the following: a-z, A-Z, \" ? \", \" ! \", \" \' \" or \" . \"");
+                phrase = "DON'T LET THE GUESSER SEE IT!";
+                input = "__invalid__";
+                break;
+            }
         }
     }
     document.getElementById("phrase").innerHTML = phrase;
     document.getElementById("input").value = "";
 }
 
-function guess(id) {
-    input = input.toUpperCase();
-    if (input.includes(id)) {
-        alert("Yay!");
+function reset() {
+    for (var i = 65; i <= 90; i++) {
+        document.getElementById(String.fromCharCode(i)).disabled = false;
     }
-    alert(id);
-    alert(input);
-    document.getElementById(id).disabled = true;
+}
+
+function guess(id) {
+    if (input === "__invalid__") {
+        alert("Da hell u doin'");
+    }
+    else {
+        input = input.toUpperCase();
+        if (input.includes(id)) {
+            alert("Yay!");
+        }
+        alert(id);
+        alert(input);
+        document.getElementById(id).disabled = true;
+    }
 }
 
 /* Call listed functions after html loads */
