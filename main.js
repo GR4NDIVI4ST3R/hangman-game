fdee68d07fea5167926e648ab6d65a24b691d384
GR4NDIVI4ST3R <ihsxaviorp@gmail.com>
2019-12-04 21:38:49 -0800

    Hide submit and implementation of solution

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-var resetPhrase = "DON'T LET THE GUESSER SEE IT!";
-var phrase = resetPhrase;
-var input = "__invalid__";
+var resetMessage = "DON'T LET THE GUESSER SEE IT!";
+var winMessage = "You Won!"
+var phrase = resetMessage;
+var input = "__INVALID__"; // Default when nothing is inside input box
+var solution = ""; // Default when nothing is inside input box
 
-/* Paste in full A-Z keyboard using ASCII Codes, instead of manual HTML */
-/* Also much easier to make edits to keyboard buttons */
+//TODO: make incorrect button guesses do something
+
+// Paste in full A-Z keyboard using ASCII Codes, instead of manual HTML
+// Also much easier to make edits to keyboard buttons
 function keyboard() {
     var layout = "";
-    var box = "<div class=\"checkbox-container\"><button class=\"checkbox\" id=\""; /*Easy to edit Ex: adding onclick(), changing class names, etc*/
+    var box = "<div class=\"checkbox-container\"><button class=\"checkbox\" id=\""; // Easy to edit Ex: adding onclick(), changing class names, etc
     for (var i = 65; i <= 90; i++) {
         layout += box + String.fromCharCode(i) + "\" onclick=\"guess(this.id)\"> " + String.fromCharCode(i) + "</button></div>";
     }
@@ -16,17 +20,19 @@ function keyboard() {
 function submit() {
     phrase = "";
     input = document.getElementById("input").value;
+    input = input.toUpperCase();
+    // Check if input box is empty
     if (input.trim().length === 0) {
         alert("Invalid Phrase; Please use the following: [ a-z ], [ A-Z ], [ – ], [ . ], [ \' ], [ ? ] or [ ! ]");
-        phrase = "DON'T LET THE GUESSER SEE IT!";
-        input = "__invalid__";
+        reset();
     }
     else {
+        solution = input;
         for (var i = 0; i < input.length; i+= 1) {
-            if (input.charAt(i).match(/^[a-zA-Z]+$/)) { /* The /^[]+$/ is Javascript Regex */
+            if (input.charAt(i).match(/^[a-zA-Z]+$/)) { // The /^[]+$/ is Javascript Regex
                 phrase += "___ ";
             }
-            /* If [else if] isn't used, the last [else] will only apply to the [if] above it */
+            // If [else if] isn't used, the last [else] will only apply to the [if] above it
             else if (input.charAt(i) == " ") {
                 phrase += "     ";
             }
@@ -48,30 +54,39 @@ function submit() {
             else if (input.charAt(i) == "!") {
                 phrase += "! ";
             }
+            // If input contains any characters not listed above, do the following:
             else {
                 alert("Invalid Phrase; Please use the following: a-z, A-Z, \" ? \", \" ! \", \" \' \" or \" . \"");
                 phrase = "DON'T LET THE GUESSER SEE IT!";
-                input = "__invalid__";
+                input = "__INVALID__";
                 break;
             }
         }
+        document.getElementById("phrase").innerHTML = phrase;
+        document.getElementById("input").value = "";
+        document.getElementById("input").classList.toggle("hide");
+        document.getElementById("submit").classList.toggle("hide");
     }
-    document.getElementById("phrase").innerHTML = phrase;
-    document.getElementById("input").value = "";
 }
 
 
 function guess(id) {
-    if (input === "__invalid__") {
+    if (solution === "__INVALID__") {
         alert("Da hell u doin'");
     }
     else {
-        input = input.toUpperCase();
-        if (input.includes(id)) {
-            alert("Yay!");
+        // Remove every instance of the btn's letter from solution
+        while (solution.includes(id)) {
+            solution = solution.replace(id,"");
         }
-        alert(id);
+        //* Win Condition
+        if (solution.trim().length === 0) {
+            document.getElementById("phrase").innerHTML = winMessage;
+        }
+        alert(solution);
+        /* Debugging:
         alert(input);
+        */
         document.getElementById(id).disabled = true;
     }
 }
@@ -81,12 +96,15 @@ function reset() {
         document.getElementById(String.fromCharCode(i)).disabled = false;
     }
     document.getElementById("input").value = "";
-    document.getElementById("phrase").innerHTML = resetPhrase;
+    document.getElementById("phrase").innerHTML = resetMessage;
+    document.getElementById("input").classList.remove("hide");
+    document.getElementById("submit").classList.remove("hide");
     phrase = "";
-    input = "__invalid__";
+    input = "__INVALID__";
+    solution = "__INVALID__";
 }
 
-/* Call listed functions after html loads */
+// Call listed functions after html loads
 function onload() {
     keyboard();
 }
