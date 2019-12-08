402e7c2905b64c5a13804e936c4dda6f6689a1b0
GR4NDIVI4ST3R <ihsxaviorp@gmail.com>
2019-12-08 14:21:21 -0800

    Everything done except gallows and phrase box

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var resetMessage = "DON'T LET THE GUESSER SEE IT!";
-var winMessage = "You Won!"
+//* Global Variables
 var phrase = resetMessage;
-var input = "__INVALID__"; // Default when nothing is inside input box
+var input = "__INVALID__"; // Used for when nothing is inside input box
 var solution = ""; // Default when nothing is inside input box
+var bodyParts = 0;
+var resetMessage = "DON'T LET THE GUESSER SEE IT!";
+var winMessage = "You Won!";
+var loseMessage = "You Lose! lmao!";
+var debug = "";
 
-//TODO: make incorrect button guesses do something
+//TODO: make gallows
+//TODO: make underscores turn into letters when guess is correct
+//TODO: make git thing with batch
 
 // Paste in full A-Z keyboard using ASCII Codes, instead of manual HTML
 // Also much easier to make edits to keyboard buttons
@@ -27,7 +33,17 @@ function submit() {
         reset();
     }
     else {
+        //* Format input to be used properly as solution
         solution = input;
+        solution = solution.replace("'", "");
+        solution = solution.replace("-", "");
+        solution = solution.replace("–", "");
+        solution = solution.replace(".", "");
+        solution = solution.replace(",", "");
+        solution = solution.replace("?", "");
+        solution = solution.replace("!", "");
+        
+        //* Make input phrase hidden with underscores
         for (var i = 0; i < input.length; i+= 1) {
             if (input.charAt(i).match(/^[a-zA-Z]+$/)) { // The /^[]+$/ is Javascript Regex
                 phrase += "___ ";
@@ -75,33 +91,80 @@ function guess(id) {
         alert("Da hell u doin'");
     }
     else {
-        // Remove every instance of the btn's letter from solution
-        while (solution.includes(id)) {
-            solution = solution.replace(id,"");
-        }
-        //* Win Condition
-        if (solution.trim().length === 0) {
-            document.getElementById("phrase").innerHTML = winMessage;
-        }
-        alert(solution);
-        /* Debugging:
-        alert(input);
-        */
+        //* Disable button when guess is made
+        document.getElementById(id).classList.toggle("hide");
         document.getElementById(id).disabled = true;
+
+        //* Correct Guess
+        if (solution.includes(id)) {
+            // Show guess in phrase box
+                // Find every location of id in input and translate to location of id in phrase
+            for (var i=0; i == 10; i++) {
+                /* Translate between input.length and var i
+                using a scale based on the underscores being four positions long */
+                // Account for apostrophe and other punctuation
+                // Maybe use a dictionary:
+                    // Store the id's as variables and their positions as definitions
+
+            }
+
+            ////phrase = phrase.replace("___ ___ ", " V   A   S   E       ___ ___ ")
+            document.getElementById("phrase").innerHTML = phrase;
+
+            // Remove every instance of the guess from solution
+            while (solution.includes(id)) {
+                solution = solution.replace(id,"");
+            }
+
+            document.getElementById("phrase").innerHTML = phrase;
+        }
+        //* Incorrect guess
+        else if (bodyParts < 9) {
+            bodyParts++;
+        }
+
+        //* Win condition
+        if (solution.trim().length === 0) {
+            alert(winMessage);
+            reset();
+            return "won"; //Exits reset function; "won" is for possible later use
+        }
+        
+        //* Lose condition
+        if (bodyParts >= 9) {
+            alert(loseMessage);
+            alert("Answer: " + input);
+            reset();
+            return "lost"; //Exits reset function; "lost" is for possible later use
+        }
+
+        //* Debugging
+        debug = "Solution: " + solution + "\n" /*New Line*/ +
+                "Answer: " + input + "\n" + 
+                "Body Parts: " + bodyParts;
+        alert(debug); //Toggle using strikethrough
     }
 }
 
 function reset() {
+    //* Reset Buttons
     for (var i = 65; i <= 90; i++) {
+        // String.fromCharCode(i) = button id "A, B, C...""
         document.getElementById(String.fromCharCode(i)).disabled = false;
+        document.getElementById(String.fromCharCode(i)).classList.remove("hide");
     }
-    document.getElementById("input").value = "";
-    document.getElementById("phrase").innerHTML = resetMessage;
+    //* Show and reset input box, submit btn, and phrase
     document.getElementById("input").classList.remove("hide");
     document.getElementById("submit").classList.remove("hide");
-    phrase = "";
+
+    document.getElementById("input").value = "";
     input = "__INVALID__";
+
+    document.getElementById("phrase").innerHTML = resetMessage;
+    phrase = "";
+
     solution = "__INVALID__";
+    bodyParts = 0;
 }
 
 // Call listed functions after html loads
